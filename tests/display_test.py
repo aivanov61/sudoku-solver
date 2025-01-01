faf77b708761ba4d9911c2a349664b2107c0cba6
Anton Ivanov <antonivanov1961@gmail.com>
2025-01-01 14:25:59 -0800

    Apply non-functional cleanup based on code re-review

--- tests/display_test.py
+++ tests/display_test.py
@@ -1,7 +1,18 @@
 # -----------------------------------------------------------------------------
 # Copyright (c) by Anton Ivanov.
-# This information is the confidential and proprietary property of Anton Ivanov
-# and the possession or use of this file requires a written license from him.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # -----------------------------------------------------------------------------
 
 import random
@@ -18,22 +29,23 @@ class TestDisplay(TestCase):
     Display.geometry({"h_cells": 9, "v_cells": 9})
 
     def setUp(self):
-        pass
+        return super().setUp()
+
+    def tearDown(self):
+        Display.term.inkey(timeout=1.0)
+        return super().tearDown()
 
     def test_screen_size_validation(self):
         Display.validate_screen_size()
-        Display.term.inkey(timeout=1.0)
 
     def test_draw_puzzle(self):
         for row in range(3):
             for col in range(3):
                 self.draw_block(row, col)
         self.fill_blocks()
-        Display.term.inkey(timeout=1.0)
 
     def test_warn(self0):
         Display.warn("Testing - this line should be yellow")
-        Display.term.inkey(timeout=1.0)
 
     def draw_block(self, block_row, block_col):
         Display.draw_cell(row=block_row * 3 + 0, col=block_col * 3 + 0, border=Border({Border.TOP, Border.LEFT}))
